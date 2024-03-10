0417efe9320adcfe82de2dbe8acf362631bf09c0
Jaysenso <jsenongxuan@gmail.com>
2024-03-11 02:36:05 +0800

    Created LeetCode Title Formatter

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let userInput = document.getElementById("userInput");
+let form = document.getElementById("formatterForm");
+let languageSelector = document.getElementById("languageSelector");
+let result = document.getElementById("resultText");
+
+const languageDict = {
+  JavaScript: "js",
+  Java: "java",
+  Python: "py",
+  C: "c",
+};
+
+form.addEventListener("submit", function (event) {
+  result.textContent = "";
+  event.preventDefault();
+  const title = userInput.value.trim().replace(/[.]/g, "");
+  const selectedLanguage = languageSelector.value;
+  const languageExtension = languageDict[selectedLanguage];
+
+  let formattedTitle = title.replace(/[.]/g, "");
+  formattedTitle = title.replace(/[\s+]/g, "_");
+  result.textContent = formattedTitle + "." + languageExtension;
+});
